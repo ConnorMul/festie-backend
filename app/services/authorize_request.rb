ee4cb9df245492fc17ceba2be3e5347340a95fdf
ConnorMul <mulhollandconnor@icloud.com>
2021-03-03 19:04:03 -0500

    changes

--- app/services/authorize_request.rb
+++ app/services/authorize_request.rb
@@ -12,7 +12,7 @@ class AuthorizeRequest
     attr_reader :headers
 
     def decoded_token
-      secret = ENV[JWT_SECRET_KEY]
+      secret = ENV['JWT_SECRET_KEY']
       JWT.decode(auth_token, secret, true, { algorthim: 'HS256' })[0]
     rescue
       nil
